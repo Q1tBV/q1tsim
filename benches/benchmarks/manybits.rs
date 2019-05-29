d83a974292b66454f6b3d9acf9d641cbc8d9cc0e
Gé Vissers <ge@q1t.nl>
2019-05-29 19:34:04 +0200

    More reasonable parameters for manybits benchmark, add vector state benchmark for comparison

--- benches/benchmarks/manybits.rs
+++ benches/benchmarks/manybits.rs
@@ -3,17 +3,15 @@ use q1tsim::circuit::{QuStateRepr, Circuit};
 
 use rand_core::SeedableRng;
 
-fn build_manybits_circuit(nr_bits: usize) -> q1tsim::error::Result<Circuit>
+fn build_manybits_circuit(nr_bits: usize, nr_gates: usize) -> q1tsim::error::Result<Circuit>
 {
     let mut circuit = Circuit::new(nr_bits, 2);
 
-    for i in 0..nr_bits-1
+    for i in 0..nr_gates
     {
-        circuit.h(i)?;
-        circuit.cx(i, i+1);
+        circuit.h(i % nr_bits)?;
+        circuit.cx(i % nr_bits, (i+1) % nr_bits)?;
     }
-    circuit.h(nr_bits-1)?;
-    circuit.cx(nr_bits, 0);
 
     circuit.measure(0, 0)?;
     circuit.measure(nr_bits-1, 1)?;
@@ -21,11 +19,11 @@ fn build_manybits_circuit(nr_bits: usize) -> q1tsim::error::Result<Circuit>
     Ok(circuit)
 }
 
-fn manybits(nr_bits: usize, nr_shots: usize, stabilizer: bool)
+fn manybits(nr_bits: usize, nr_gates: usize, nr_shots: usize, stabilizer: bool)
 {
     let mut rng = rand_hc::Hc128Rng::seed_from_u64(0x1f67a51423cd2615);
 
-    let mut circuit = build_manybits_circuit(nr_bits).expect("Failed to build circuit");
+    let mut circuit = build_manybits_circuit(nr_bits, nr_gates).expect("Failed to build circuit");
     if stabilizer
     {
         circuit.execute_with_rng(nr_shots, &mut rng).expect("Failed to execute circuit");
@@ -39,17 +37,17 @@ fn manybits(nr_bits: usize, nr_shots: usize, stabilizer: bool)
 
 fn bench_manybits(c: &mut Criterion)
 {
-    c.bench_function("mb vec 10", |b| b.iter(|| manybits(10, 1_000, false)));
-    c.bench_function("mb vec 11", |b| b.iter(|| manybits(11, 1_000, false)));
-    c.bench_function("mb vec 12", |b| b.iter(|| manybits(12, 1_000, false)));
+    c.bench_function("mb vec 10", |b| b.iter(|| manybits(10, 100, 1_000, false)));
+    c.bench_function("mb vec 11", |b| b.iter(|| manybits(11, 100, 1_000, false)));
+    c.bench_function("mb vec 12", |b| b.iter(|| manybits(12, 100, 1_000, false)));
 
-    c.bench_function("mb stab 10", |b| b.iter(|| manybits(10, 1_000, true)));
-    c.bench_function("mb stab 11", |b| b.iter(|| manybits(11, 1_000, true)));
-    c.bench_function("mb stab 12", |b| b.iter(|| manybits(12, 1_000, true)));
-    c.bench_function("mb stab 20", |b| b.iter(|| manybits(20, 1_000, true)));
-    c.bench_function("mb stab 35", |b| b.iter(|| manybits(35, 1_000, true)));
-    c.bench_function("mb stab 50", |b| b.iter(|| manybits(50, 1_000, true)));
-    c.bench_function("mb stab 100", |b| b.iter(|| manybits(100, 1_000, true)));
+    c.bench_function("mb stab 10", |b| b.iter(|| manybits(10, 100, 1_000, true)));
+    c.bench_function("mb stab 11", |b| b.iter(|| manybits(11, 100, 1_000, true)));
+    c.bench_function("mb stab 12", |b| b.iter(|| manybits(12, 100, 1_000, true)));
+    c.bench_function("mb stab 20", |b| b.iter(|| manybits(20, 100, 1_000, true)));
+    c.bench_function("mb stab 35", |b| b.iter(|| manybits(35, 100, 1_000, true)));
+    c.bench_function("mb stab 50", |b| b.iter(|| manybits(50, 100, 1_000, true)));
+    c.bench_function("mb stab 100", |b| b.iter(|| manybits(100, 100, 1_000, true)));
 }
 
 criterion_group!(benches, bench_manybits);
